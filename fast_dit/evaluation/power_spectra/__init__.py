926ea87bd2d1b91487e41b80bff0ef34281fb8de
Sudarsaan <sudarsaan@itglobe.com>
2024-08-18 01:06:45 +0100

    Fixed some relative imports

--- fast_dit/evaluation/power_spectra/__init__.py
+++ fast_dit/evaluation/power_spectra/__init__.py
@@ -1,3 +1,3 @@
-from power_spectra_eval import get_power_spectra, compare_power_spectra
+from .power_spectra_eval import get_power_spectra, compare_power_spectra
 
 __all__ = ['get_power_spectra', 'compare_power_spectra'] 
