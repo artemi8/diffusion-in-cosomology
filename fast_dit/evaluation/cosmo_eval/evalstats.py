b7106769df185c2b1fc648d2b6e00b25009b5281
Sudarsaan <sudarsaan@itglobe.com>
2024-08-30 21:30:06 +0100

    Solved a error in Pylians power spectrum calculation code

--- fast_dit/evaluation/cosmo_eval/evalstats.py
+++ fast_dit/evaluation/cosmo_eval/evalstats.py
@@ -128,7 +128,7 @@ def get_powspec_for_samples(samplist, box_size=1000, MAS='CIC'):
             # assert samp.shape[-1]==Nx
             # assert samp.shape[-2]==Nx
             Pk2D = PKL.Pk_plane(single_samp.astype('float32'), BoxSize, MAS, threads)
-            k_vals = Pk2D.k      #k in h/Mpc
+            kvals = Pk2D.k      #k in h/Mpc
             temp_pk.append(Pk2D.Pk)     #Pk in (Mpc/h)^2
         # ps_list.append(np.mean(np.array(temp_pk), axis=0))
         ps_list.append(temp_pk)
